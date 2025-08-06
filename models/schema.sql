28c6085be09468d5cd24b38afb5ce3611f9d3bb7
TEJA MAJETI <tejasaimanikanta2004@gmail.com>
2025-08-06 12:26:45 +0530

    Modified Schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,31 +1,32 @@
+-- Users Table
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY ,
+  id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
-  role TEXT CHECK(role IN ('mentor', 'learner')) NOT NULL,
+  role TEXT CHECK (role IN ('mentor', 'learner')) NOT NULL,
   bio TEXT
 );
 
-
+-- Skills Table
 CREATE TABLE IF NOT EXISTS skills (
-  id  SERIAL PRIMARY KEY ,
+  id SERIAL PRIMARY KEY,
   mentor_id INTEGER NOT NULL,
   title TEXT NOT NULL,
   description TEXT,
   tags TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY(mentor_id) REFERENCES users(id)
+  FOREIGN KEY (mentor_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+-- Bookings Table
 CREATE TABLE IF NOT EXISTS bookings (
-  id SERIAL PRIMARY KEY ,
+  id SERIAL PRIMARY KEY,
   skill_id INTEGER NOT NULL,
   learner_id INTEGER NOT NULL,
-  status TEXT DEFAULT 'pending', 
+  status TEXT CHECK (status IN ('pending', 'accepted', 'rejected')) DEFAULT 'pending',
   message TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY(skill_id) REFERENCES skills(id), 
-  FOREIGN KEY(learner_id) REFERENCES users(id)
+  FOREIGN KEY (skill_id) REFERENCES skills(id) ON DELETE CASCADE,
+  FOREIGN KEY (learner_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
